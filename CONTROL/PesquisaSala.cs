5c933ba78af748bd23d38a7a28dddc75472324be
Daniel <dever.daniel.asf@gmail.com>
2023-07-06 16:10:15 -0300

    classe para pesquisa de sala, teste ensina a usar

---
+++ CONTROL/PesquisaSala.cs
@@ -0,0 +1,57 @@
+﻿using MODEL;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3;
+
+namespace CONTROL
+{
+    public class PesquisaSala:conexao
+    {
+        
+        MySqlCommand cmd = null;
+        MySqlDataReader dr = null;
+        m_sala m_sala = null;
+        string sql;
+        public IEnumerable<m_sala> Pesquisar(string sala)
+        {
+            try
+            {
+                conn = getconexao();
+                conn.Open();
+                sql = "select * from tb_sala_do_predio where nome_sala like @sala"; 
+                cmd = new MySqlCommand(sql, conn);
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@sala", sala+"%");
+                dr = cmd.ExecuteReader();
+            }
+            catch
+            {
+                
+            }
+            
+            while (dr.Read())
+            {
+                m_sala = new m_sala();
+                m_sala.setnome_sala(dr.GetString("nome_sala"));
+                m_sala.setbloco_sala(dr.GetString("bloco"));
+                m_sala.setdescricao_sala(dr.GetString("descricao_sala"));
+                m_sala.setnumeracao_sala(dr.GetString("numeracao"));
+                yield return m_sala;
+            } 
+
+            
+           
+            
+            
+
+            
+            
+        }
+
+    }
+}
---
+++ Projeto Loc Senai/teste.cs
@@ -0,0 +1,34 @@
+﻿using CONTROL;
+using MODEL;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Loc_Senai
+{
+    public partial class teste : Form
+    {
+        public teste()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PesquisaSala pesquisa = new PesquisaSala();
+            IEnumerable<m_sala> salas = pesquisa.Pesquisar("a%");
+            foreach(m_sala sala in salas){
+                string teste = sala.Getnome_sala();
+                MessageBox.Show(teste.ToString());
+            }
+
+        }
+    }
+}
